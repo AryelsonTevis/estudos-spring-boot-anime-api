4e4209367692e944767258e092709a67e667734c
Aryelson Tevis <tevisaryelson@gmail.com>
2026-09-22 16:34:54 -0300

    Estudos sobre paginacao e RestTemplate

---
+++ src/main/java/estudos/spring/EstudosSpringBoot/client/SpringClient.java
@@ -0,0 +1,32 @@
+package estudos.spring.EstudosSpringBoot.client;
+
+import estudos.spring.EstudosSpringBoot.domain.Anime;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+public class SpringClient {
+    static void main(String[] args) {
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/6", Anime.class);
+        log.info(entity);
+
+        Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 6);
+        log.info(object);
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+        log.info(Arrays.toString(animes));
+
+        ResponseEntity<List<Anime>> animesList = new RestTemplate()
+                .exchange("http://localhost:8080/animes/all", HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<>() {
+                        });
+        log.info(animesList.getBody());
+    }
+}
--- src/main/java/estudos/spring/EstudosSpringBoot/repository/AnimeRepository.java
+++ src/main/java/estudos/spring/EstudosSpringBoot/repository/AnimeRepository.java
@@ -1,13 +1,15 @@
 package estudos.spring.EstudosSpringBoot.repository;
 
-import estudos.spring.EstudosSpringBoot.domain.Anime;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import estudos.spring.EstudosSpringBoot.domain.Anime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
 
-    List<Anime> findByName(String name);
+    Page<Anime> findAll( Pageable pageable);
+    Page<Anime> findByName(String name, Pageable pageable);
 
 }
